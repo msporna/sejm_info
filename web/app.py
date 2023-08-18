68e836206b8566240a1420b35cc1d4d55605eeea
Michal Sporna <michal.sporna@priva.com>
2023-08-18 17:32:59 +0200

    styling updates

--- web/app.py
+++ web/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-last_update = "2023/08/17"  # todo  UPDATE THIS DATE AFTER EACH UPDATE
+last_update = "2023/08/18"  # todo  UPDATE THIS DATE AFTER EACH UPDATE
 
 title = "Sejm Info"
 sqlite_handler = SqliteHandler()
@@ -37,6 +37,7 @@ def details(item_id):
     for s in summaries:
         if s["id"] == item_id:
             return render_template("details.html", item_id=item_id, title=title, summary=s["summary"],
+                                   long_summary=s["long_summary"],
                                    project_id=s["project_id"], url=s["url"], process_url=s["process_url"],
                                    document_date=s["document_date"])
     return render_template("details.html", item_id=item_id, title=title, summary="N/A", url="N/A")
